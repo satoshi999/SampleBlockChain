20f643182280f86a16fe9b1472890e544e74f68e
Satoshi Hirano <satoshi.hiranoz@gmail.com>
2020-02-16 16:52:51 +0900

    docker対応&global ip取得

--- p2p/signalingClient.js
+++ p2p/signalingClient.js
@@ -1,15 +1,14 @@
 class SignalingClient{
   constructor() {
     const client = require('./client');
-    const constants = require('../config/constants');
     const connect = () => {
-      const socket = require('socket.io-client').connect(`http://${process.env.SIG_SERVER}:3000`, {query: `ip=${constants.IP}`});
+      const socket = require('socket.io-client').connect(`http://${process.env.BOOT_NODE}:3000`, {query: `ip=${process.env.IP}`});
       socket.on('signaling nodes', function(nodes){
         const json = JSON.parse(nodes);
         for(let key in json) {
           const ip = json[key];
           console.log(`socketId:${key}, ip address:${ip}`);
-          if(ip !== constants.IP) {
+          if(ip !== process.env.IP) {
             client.connect(ip);
           }
         }
