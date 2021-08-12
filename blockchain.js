ec2b27e2a83637a6a22ff067da4ec76d9f540bdc
Satoshi Hirano <satoshi.hiranoz@gmail.com>
2021-08-12 16:53:43 +0900

    ファイル整理

--- blockchain.js
+++ blockchain.js
@@ -6,6 +6,7 @@ const MINING_REWORD = 1;
 
 class BlockChain {
   constructor(_address) {
+    this.wallets = {};
     this.transactionPool = [];
     this.chain = [];
     this.createBlock(0, this.hash(GENESIS_ADDRESS));
@@ -40,19 +41,15 @@ class BlockChain {
   }
 
   validTxs(txs) {
-    this.wallets = {};
-    this.wallets[GENESIS_ADDRESS] = TOTAL_SUPPLY;
     return txs.every((tx) => {
-      if(!this.wallets[tx.senderAddress]) {
+      const senderAmount = (tx.senderAddress in this.wallets)? Number(this.wallets[tx.senderAddress]) : 0;
+      const recipientAmount = (tx.recipientAddress in this.wallets)? Number(this.wallets[tx.recipientAddress]) : 0;
+      const amount = Number(tx.amount)
+      if(senderAmount - amount < 0) {
         return false;
       }
-      const senderAmount = Number.isInteger(this.wallets[tx.senderAddress])? this.wallets[tx.senderAddress] : 0;
-      const recipientAmount = Number.isInteger(this.wallets[tx.recipientAddress])? this.wallets[tx.recipientAmount] : 0;
-      if((senderAmount - tx.amount < 0) {
-        return false;
-      }
-      this.wallets[tx.senderAddress] = senderAmount - tx.amount;
-      this.wallets[tx.recipientAddress] = recipientAmount + tx.amount;
+      this.wallets[tx.senderAddress] = senderAmount - amount;
+      this.wallets[tx.recipientAddress] = recipientAmount + amount;
       return true;
     });
   }
@@ -67,17 +64,29 @@ class BlockChain {
     return guessHash.slice(0, this.continuousHeaderLength) === '0'.repeat(this.continuousHeaderLength);
   }
 
-  validChain(chain) {
-    let _previousHash = this.hash(GENESIS_ADDRESS);
-    return chain.every((block) => {
+  validChain() {
+    this.wallets = {};
+    this.wallets[GENESIS_ADDRESS] = TOTAL_SUPPLY;
+
+    const genesisHash = this.hash(GENESIS_ADDRESS);
+    let _previousHash = genesisHash;
+
+    return this.chain.every((block) => {
+      if(block.previousHash === genesisHash) {
+        _previousHash = this.hash(block);
+        return true;
+      }
       if(block.previousHash !== _previousHash) {
         return false;
       }
-      if(!this.validProof(block.transactions, _previousHash, block.nonce)) {
+      if(!this.validProof(block.transactions, block.previousHash, block.nonce)) {
+        return false;
+      }
+      if(!this.validTxs(block.transactions)) {
         return false;
       }
       _previousHash = this.hash(block);
-      return this.validTxs(block.transactions);
+      return true
     });
   }
 
@@ -92,18 +101,27 @@ class BlockChain {
   }
 
   mining() {
-    console.log("mining start");
     this.addTransaction(GENESIS_ADDRESS, this.address, MINING_REWORD);
     console.log(`added mining transaction pool:${JSON.stringify(this.transactionPool)}`);
     const {nonce, previousHash} = this.proofOfWork();
     console.log(`success proofOfWork. nonce:${nonce}, previousHash: ${previousHash}`);
     this.createBlock(nonce, previousHash);
-    console.log(`success create block. chain:${JSON.stringify(this.chain)}, transaction pool:${JSON.stringify(this.transactionPool)}`);
-    this.validChain();
-    console.log(`success valid chain. wallet:${JSON.stringify(this.wallets)}`);
+    console.log(`success create block. chain:${JSON.stringify(this.chain)}`);
+    if(this.validChain()) {
+      console.log(`success valid chain. My wallet token:${JSON.stringify(this.wallets[this.address])}`);
+    } else {
+      console.log(`Invalid chain. My wallet token:${JSON.stringify(this.wallets[this.address])}`);
+    }
     return true;
   }
 }
 
 const blockChain = new BlockChain("0x8F0325E1135cA2cC50906185D0Adf62E0274cb8D");
-blockChain.mining();
+
+console.log("mining start");
+
+while(true) {
+  blockChain.mining();
+}
+
+
